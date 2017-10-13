fa6233943577a01d346fa78fba5f6006b479210d
Paul Soberanes <paul.soberanes@omnilife.com>
2017-10-13 11:56:14 -0500

    Declaring classes

---
+++ com/omnilife/paul/classes/Box.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.omnilife.paul.classes;
+
+/**
+ *
+ * @author Paul Soberanes <paul.soberanes@omnilife.com>
+ */
+public class Box {
+
+    double width;
+    double height;
+    double depth;
+}
